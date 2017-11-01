4816d0937e08eb8f0b1e4c37b5a237330e9e9ae4
Kelyan_CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2017-11-01 09:43:27 -0400

    liste des temperatures

---
+++ ServiceWeb/temperature/liste/index.php
@@ -0,0 +1,25 @@
+<?php
+
+	header("Content-type: text/xml");
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+	
+	include "../../basededonnees.php";
+	$SQL_LISTE_TEMPERATURES = "SELECT * FROM todo";
+	$requeteListeTemperatures = $basededonnees->prepare($SQL_LISTE_TEMPERATURES);
+	$requeteListeTemperatures->execute();
+	$listeTemperatures = $requeteListeTemperatures->fetchAll(PDO::FETCH_OBJ);
+?>
+<listeTemperatures>
+<?php
+foreach($listeTemperatures as $temp)
+{
+?>
+	<Temperature>
+		<temperature><?=$temperature->temperature_celsius?></titre>
+		<date><?=$temperature->temperature_date?></date>
+		<heure><?=$temperature->temperature_heure?></heure>
+	</Temperature>
+<?php
+}
+?>
+</listeTemperatures>
