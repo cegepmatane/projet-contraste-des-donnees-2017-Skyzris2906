ecff6c5f63bd8210af01d007e08efa7702d4a3af
Kelyan_CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2017-11-02 00:18:54 -0400

    modification

--- ServiceWeb/temperature/liste/index.php
+++ ServiceWeb/temperature/liste/index.php
@@ -4,7 +4,7 @@
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
 	
 	include "../../basededonnees.php";
-	$SQL_LISTE_TEMPERATURES = "SELECT * FROM todo";
+	$SQL_LISTE_TEMPERATURES = "SELECT * FROM temperature";
 	$requeteListeTemperatures = $basededonnees->prepare($SQL_LISTE_TEMPERATURES);
 	$requeteListeTemperatures->execute();
 	$listeTemperatures = $requeteListeTemperatures->fetchAll(PDO::FETCH_OBJ);
