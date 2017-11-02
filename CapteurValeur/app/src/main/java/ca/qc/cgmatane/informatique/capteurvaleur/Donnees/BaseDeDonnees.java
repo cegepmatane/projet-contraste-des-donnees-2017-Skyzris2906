1afdd3d7dd32c9118458ff87ae01082ea5729092
max7703 <max7703@live.fr>
2017-11-01 20:39:24 -0400

    ajout BDD + valeur toute les 30min

---
+++ CapteurValeur/app/src/main/java/ca/qc/cgmatane/informatique/capteurvaleur/Donnees/BaseDeDonnees.java
@@ -0,0 +1,57 @@
+package ca.qc.cgmatane.informatique.capteurvaleur.Donnees;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BaseDeDonnees extends SQLiteOpenHelper
+{
+
+    private static BaseDeDonnees instance = null;
+
+    public static BaseDeDonnees getInstance(Context contexte)
+    {
+        if (null == instance) instance = new BaseDeDonnees(contexte);
+        return instance;
+    }
+
+    public static BaseDeDonnees getInstance()
+    {
+        return instance;
+    }
+
+    public BaseDeDonnees(Context contexte)
+    {
+        super(contexte, "capteurvaleur.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS accelerometre" +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "x DOUBLE, " +
+                "y DOUBLE, " +
+                "z DOUBLE, " +
+                "date TEXT, " +
+                "heure TEXT)";
+
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int arg1, int arg2)
+    {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db)
+    {
+       // String INSERT_1 = "INSERT INTO accelerometre(id, x, y, z, date, heure) " +
+                //"VALUES('1','test x', 'test y', 'test z', 'test date', 'test heure')";
+
+        //db.execSQL(INSERT_1);
+    }
+}
--- CapteurValeur/app/src/main/java/ca/qc/cgmatane/informatique/capteurvaleur/VueCapteur.java
+++ CapteurValeur/app/src/main/java/ca/qc/cgmatane/informatique/capteurvaleur/VueCapteur.java
@@ -1,17 +1,35 @@
 package ca.qc.cgmatane.informatique.capteurvaleur;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import ca.qc.cgmatane.informatique.capteurvaleur.Donnees.BaseDeDonnees;
 
 public class VueCapteur extends AppCompatActivity implements SensorEventListener {
     SensorManager mSensorManager = null;
     Sensor mAccelerometer = null;
     TextView valeur;
+    public static final int MY_PERMISSIONS_REQUEST_STORAGE = 999;
+    BaseDeDonnees accesseurBaseDeDonnees;
+    boolean write = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +39,12 @@ public class VueCapteur extends AppCompatActivity implements SensorEventListener
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        checkStoragePermission();
+
         valeur = (TextView)findViewById(R.id.valeur_capteur);
 
+        accesseurBaseDeDonnees = BaseDeDonnees.getInstance(getApplicationContext());
+        Log.d("INSTANCE", "Bdd = " + accesseurBaseDeDonnees);
     }
 
     @Override
@@ -40,15 +62,117 @@ public class VueCapteur extends AppCompatActivity implements SensorEventListener
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        float x = sensorEvent.values[0];
-        float y = sensorEvent.values[1];
-        float z = sensorEvent.values[2];
+        Calendar calendar = Calendar.getInstance();
 
-        valeur.setText("x:" + x + "y:" + y + "z:" + z);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        final String strDate = dateFormat.format(calendar.getTime());
+
+        final String strTemps = calendar.getTime().getHours() + ":" + calendar.getTime().getMinutes();
+
+        final float x = sensorEvent.values[0];
+        final float y = sensorEvent.values[1];
+        final float z = sensorEvent.values[2];
+
+        valeur.setText(" x: " + x + " y: " + y + " z: " + z + " date: " + strDate + " heure: " + strTemps + " seconds: " + calendar.getTime().getSeconds());
+
+        if(calendar.getTime().getSeconds() == 0 && (calendar.getTime().getMinutes() == 30 || calendar.getTime().getMinutes() == 0))
+        {
+            if(!write)
+            {
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        String AJOUTER_LA_VALEUR = "INSERT INTO accelerometre(x, y, z, " +
+                                "date, heure) VALUES('" + x +
+                                "', '" + y + "', " +
+                                "'" + z + "', " +
+                                "'" + strDate + "', " +
+                                "'" + strTemps + "')";
+
+                        Log.d("INSERT", AJOUTER_LA_VALEUR);
+
+                        accesseurBaseDeDonnees.getWritableDatabase().execSQL(AJOUTER_LA_VALEUR);
+
+                        Toast.makeText(VueCapteur.this, "Valeur ajouté à la BDD", Toast.LENGTH_LONG).show();
+                        write = true;
+                    }
+                }, 1);
+            }
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    private void checkStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Storage Permission Needed")
+                        .setMessage("This app needs the Storage permission, please accept to use storage functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(VueCapteur.this,
+                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_STORAGE );
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else
+            {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_STORAGE );
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        //add stuff
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
