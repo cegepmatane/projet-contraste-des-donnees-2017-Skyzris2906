82d62e5e4de2a2910bd5d0a5984e0c50124afd9f
Nicolas Damonneville <SkyzrisPlayer2906@users.noreply.github.com>
2017-11-15 13:46:03 -0500

    Code Postgre sur la Raspberry

---
+++ RaspberryCode/RaspberryCode/Launcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Launcher
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Script lancé");
+        Console.WriteLine("Ecriture dans la table: \"temperature\" toutes les 30 minutes");
+        new ReadFromArduino();
+    }
+}
