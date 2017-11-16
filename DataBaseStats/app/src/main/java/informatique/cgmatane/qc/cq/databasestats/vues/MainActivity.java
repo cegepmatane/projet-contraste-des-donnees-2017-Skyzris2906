d6f6a3c8efed9e1c40e45f6c7ec0c6cf0a92dca5
lucasmarrel <marrel.lucas@gmail.com>
2017-11-16 11:19:13 -0500

    Ajout Vue Temperature et Vue Accelerometre + navigation fonctionnelle depuis le Main Activity

--- DataBaseStats/app/src/main/java/informatique/cgmatane/qc/cq/databasestats/vues/MainActivity.java
+++ DataBaseStats/app/src/main/java/informatique/cgmatane/qc/cq/databasestats/vues/MainActivity.java
@@ -1,16 +1,41 @@
 package informatique.cgmatane.qc.cq.databasestats.vues;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import informatique.cgmatane.qc.cq.databasestats.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Button boutonTemperatures;
+    protected Button boutonAccelerometre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boutonTemperatures = (Button)findViewById(R.id.bouton_temperatures_main_activity);
+        boutonAccelerometre = (Button)findViewById(R.id.bouton_accelerometre_main_activity);
+
+        boutonTemperatures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,VueTemperatures.class);
+                startActivity(intent);
+            }
+        });
+
+        boutonAccelerometre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,VueAccelerometre.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
---
+++ DataBaseStats/app/src/main/java/informatique/cgmatane/qc/cq/databasestats/vues/VueAccelerometre.java
@@ -0,0 +1,15 @@
+package informatique.cgmatane.qc.cq.databasestats.vues;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import informatique.cgmatane.qc.cq.databasestats.R;
+
+public class VueAccelerometre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_accelerometre);
+    }
+}
