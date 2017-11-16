b9ee15171faec6d9af58237796106bd0f9c76ec1
lucasmarrel <marrel.lucas@gmail.com>
2017-11-16 11:01:30 -0500

    Ajout des composants visuels du Main Activity

--- DataBaseStats/app/src/main/java/informatique/cgmatane/qc/cq/databasestats/vues/MainActivity.java
+++ DataBaseStats/app/src/main/java/informatique/cgmatane/qc/cq/databasestats/vues/MainActivity.java
@@ -1,7 +1,9 @@
-package informatique.cgmatane.qc.cq.databasestats;
+package informatique.cgmatane.qc.cq.databasestats.vues;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import informatique.cgmatane.qc.cq.databasestats.R;
 
 public class MainActivity extends AppCompatActivity {
 
