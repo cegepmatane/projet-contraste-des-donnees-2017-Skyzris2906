fc8783f1bd0c2e0cfe47b207e288473a7dcb3fb7
max7703 <max7703@live.fr>
2017-11-17 17:37:28 -0500

    ajout de la requete SQL

--- NodeJS/service-accelerometre.js
+++ NodeJS/service-accelerometre.js
@@ -5,6 +5,8 @@ var port = 8080;
 var postgresql = require('pg');
 var chaineDeConnection = 'postgres://maxime:naruto77@localhost:5432/database_principal';
 
+var SQL_AJOUTER_ACCELEROMETRE = "INSERT INTO accelerometre(x,y,z,date,heure) VALUES(':x', ':y', ':z', ':date', ':heure')";
+
 var repondre = async function(requete,reponse)
 {
 	if(requete.method === 'POST')
@@ -18,12 +20,14 @@ var repondre = async function(requete,reponse)
 					var accelerometre = JSON.parse(message); 
 					console.log(accelerometre);
 					await ajouterAccelerometre(accelerometre);
+					
 					reponse.statusCode = 200;
 					reponse.setHeader('Content-type', 'application/json');
-					reponse.end("Valeur ajoutée !");					
+					reponse.end("Valeur ajoutée !");			
 				});
-		}
+		}		
 	}
+	
 };
 
 ajouterAccelerometre = async function(accelerometre)
