b6d3c3767d8b7ff00a9f09634eefbf9d8de38a20
GauthierLory <gauthier.axel.lory@gmail.com>
2017-11-19 02:32:12 -0500

    premier jet fx

--- ApplicationTemperature/src/ApplicationService.java
+++ ApplicationTemperature/src/ApplicationService.java
@@ -12,6 +12,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
 
 import accesseur.AccelerometreDAO;
 import accesseur.TemperatureDAO;
@@ -20,13 +24,33 @@ public class ApplicationService
 {
 	public static void main (String[] args)
 	{
+		System.out.println("Lencement de l'application");
+		launch.(args);
 		
-		AccelerometreDAO accelerometreDAO = new AccelerometreDAO();
-		accelerometreDAO.listerAccelerometre();
+		//AccelerometreDAO accelerometreDAO = new AccelerometreDAO();
+		//accelerometreDAO.listerAccelerometre();
 		
-		TemperatureDAO temperatureDAO = new TemperatureDAO();
-		temperatureDAO.listerTemperature();
-		
+		//TemperatureDAO temperatureDAO = new TemperatureDAO();
+		//temperatureDAO.listerTemperature();
 	}
+	
+	public void init() {
+		System.out.println(" dans la methode init() ");
+	}
+	
+	public void start(Stage myScene) 
+	{
+		System.out.println("dans la methode start()");
 
+		myStage.setTitle("java fx test ");
+		FlowPane RootNode = new FlowPane();
+		Scene myScene = new Scene(RootNode,300,200);
+		myScene.setScene(myScene);
+		myScene.show();
+	}
+	
+	public void stop()
+	{
+		System.out.println("");
+	}
 }
