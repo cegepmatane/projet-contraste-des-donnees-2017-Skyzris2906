6ce6b49454dce6244ebf98f4a0afdc637a841cf0
GauthierLory <gauthier.axel.lory@gmail.com>
2017-11-18 21:52:50 -0500

    Model Pixel

---
+++ ApplicationTemperature/src/modele/Pixel.java
@@ -0,0 +1,43 @@
+package modele;
+
+public class Pixel {
+	
+	protected int id;
+	protected int pixel;
+	protected String date;
+	protected String heure;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getPixel() {
+		return pixel;
+	}
+
+	public void setPixel(int pixel) {
+		this.pixel = pixel;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getHeure() {
+		return heure;
+	}
+
+	public void setHeure(String heure) {
+		this.heure = heure;
+	}
+	
+}
+
