76c7a1ced700c48b49f8951015b603f19a332a89
GauthierLory <gauthier.axel.lory@gmail.com>
2017-11-18 22:04:35 -0500

    Parse XML pour Pixel

---
+++ ApplicationTemperature/src/accesseur/PixelDAO.java
@@ -0,0 +1,35 @@
+package accesseur;
+
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class PixelDAO {
+	
+	private Document parserXML(String xml)
+	{
+		Document doc = null;
+		DocumentBuilder parseur;
+		try {
+			parseur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			doc = parseur.parse(new StringBufferInputStream(xml));		
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return doc;
+	}
+
+}
